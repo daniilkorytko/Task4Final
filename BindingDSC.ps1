62887124553a9bf038ab67927f077c438a671bff
daniilkorytko <43876618+daniilkorytko@users.noreply.github.com>
2018-10-10 15:41:46 +0300

    Add files via upload

---
+++ BindingDSC.ps1
@@ -0,0 +1,27 @@
+﻿Configuration Binding
+{
+
+$sitename = "Default Web Site" 
+
+Import-DSCResource -ModuleName PSDesiredStateConfiguration 
+
+Import-DscResource -ModuleName xWebAdministration -Name MSFT_xWebsite
+
+xWebsite MainHTTPWebsite  
+{  
+    Ensure          = "Present"  
+    Name            = 'Default Web Site'
+    ApplicationPool = "DefaultAppPool" 
+    State           = "Started"  
+    PhysicalPath    = "%SystemDrive%\inetpub\wwwroot\iisstart.htm"  
+    BindingInfo     =  @(MSFT_xWebBindingInformation
+                            {
+                        Protocol              = 'HTTP' 
+                        Port                  = '8080'
+                        IPAddress             = '*'
+                        HostName              = 'localhost'
+
+                            }
+                         )   
+}
+}
