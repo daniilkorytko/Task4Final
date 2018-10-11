9047e3af1b4cfa21a1281e584cf9419ab9b01f67
daniilkorytko <43876618+daniilkorytko@users.noreply.github.com>
2018-10-11 15:59:59 +0300

    Add files via upload

---
+++ FinalDSC.ps1
@@ -0,0 +1,53 @@
+﻿Configuration IISFireWall
+{
+    param ($MachineName)
+
+    Import-DscResource -ModuleName xNetworking, xWebAdministration, PSDesiredStateConfiguration
+
+    Node $MachineName {
+
+        WindowsFeature IIS {
+            Ensure = "Present"
+            Name   = "Web-Server"
+        }
+
+        WindowsFeature ASP {
+            Ensure    = "Present"
+            Name      = "Web-Asp-Net45"
+            DependsOn = '[WindowsFeature]IIS'
+        }
+
+        WindowsFeature WebServerManagementConsole {
+            Name   = "Web-Mgmt-Console"
+            Ensure = "Present"
+        }
+        
+        xWebsite MainHTTPWebsite {  
+            Ensure          = "Present"  
+            Name            = 'Default Web Site'
+            ApplicationPool = "DefaultAppPool" 
+            State           = "Started"  
+            PhysicalPath    = 'C:\inetpub\wwwroot'  
+            BindingInfo     = @(MSFT_xWebBindingInformation {
+                    Protocol  = 'HTTP' 
+                    Port      = '8080'
+                    IPAddress = '*'
+                }
+            )
+            DependsOn       = "[WindowsFeature]ASP"    
+
+
+        }
+
+        xFirewall IIS_8080 {
+            Name        = 'IIS'
+            DisplayName = 'IIS'
+            Action      = 'Allow'
+            Direction   = 'Inbound'
+            LocalPort   = '8080'
+            Protocol    = 'TCP'
+            Profile     = 'Any'
+            Enabled     = 'True'
+        }
+    }
+}
